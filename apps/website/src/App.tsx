302267a9fa049cb92cdc9275952949f8b7374dc9
Thomas Kalve <thomas.kalve@bouvet.no>
2020-07-13 09:51:41 +0200

    Updated workflow

--- apps/website/src/App.tsx
+++ apps/website/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <h1>Diversnight</h1>;
+  return (
+    <>
+      <h1>Diversnight</h1>
+      <p>Let's go!</p>
+    </>
+  );
 }
 
 export default App;
